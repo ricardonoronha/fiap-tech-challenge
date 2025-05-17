037a3861c22ac1d1b2d6754df9269acc5c1ce4db
Matheus Costa <matheus99costa@gmail.com>
2025-05-17 18:26:14 -0300

    Modificações no DbContext e Program.cs

--- TechChallengeFIAP.Data/Repositorios/ApplicationDbContext.cs
+++ TechChallengeFIAP.Data/Repositorios/ApplicationDbContext.cs
@@ -5,36 +5,22 @@ namespace TechChallengeFIAP.Data.Repositorios
 {
     public class ApplicationDbContext : DbContext
     {
-        
-        private readonly string _connectionString;
-        
-        // Construtor da classe ApplicationDbContext vazio para que não ocorra erro ao executar as Migrations
-        public ApplicationDbContext()
-        {     
-        }
 
-        // Construtor da classe ApplicationDbContext
-        public ApplicationDbContext(string connectionString)
+        // DbContextOptions<SgtWebDb> options
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public ApplicationDbContext()
         {
-            _connectionString = connectionString;
         }
 
         public DbSet<Jogo> Jogo { get; set; }
         public DbSet<Pessoa> Pessoa { get; set; }
         public DbSet<Promocao> Promocao { get; set; }
 
-        //definição configurações do banco de dados
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Verificação se as opções do context já foram configuradas em outro lugar
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_connectionString);
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+            base.OnModelCreating(modelBuilder);
             //Configuração que permite o mapeamento de todas as classes que herdam IEntityTypeConfiguration. 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
--- TechChallengeFIAP/Program.cs
+++ TechChallengeFIAP/Program.cs
@@ -3,9 +3,9 @@ using TechChallengeFIAP.Data.Repositorios;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
+//var configuration = new ConfigurationBuilder()
+//    .AddJsonFile("appsettings.json")
+ //   .Build();
 
 // Add services to the container.
 
@@ -14,10 +14,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext <ApplicationDbContext>(options =>
+
+
+builder.Services.AddDbContext <ApplicationDbContext> (options =>
 {
-    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-}, ServiceLifetime.Scoped);
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
