baeb4dc3167ca0c09ee431975e5fe8e1d56ffb29
armandoj-oliveira <ajvdoliveira678512@gmail.com>
2025-05-31 15:57:27 -0300

    feat: adicionar validacao nas entidades pessoa e jogo

---
+++ TechChallengeFIAP.Domain/Validacao/JogoValidacao.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechChallengeFIAP.Domain.Entidades;
+
+namespace TechChallengeFIAP.Domain.Validacao
+{
+    public class JogoValidacao : AbstractValidator<Jogo>
+    {
+        public JogoValidacao()
+        {
+            RuleFor(j => j.NomeJogo)
+                .NotEmpty().WithMessage("O nome do jogo é obrigatório")
+                .MinimumLength(2);
+
+            RuleFor(j => j.DescricaoJogo)
+                .NotEmpty().WithMessage("A descrição do jogo deve ser preenchida")
+                .MinimumLength(5);
+
+            RuleFor(j => j.ClassificacaoJogo)
+                .NotEmpty().WithMessage("É necessária informar a clasificação indicativa do jogo")
+                .MinimumLength(1);
+        }
+    }
+}
