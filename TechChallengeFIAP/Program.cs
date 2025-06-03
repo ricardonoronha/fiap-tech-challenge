d2977ad0f7231b46cce2da9c89b2e8bf7567e474
Ricardo Noronha de Menezes <ricardo.noronha@outlook.com>
2025-06-02 22:45:16 -0300

    feat: migration do percentual de promocao e autenticação no swagger ui

--- TechChallengeFIAP/Program.cs
+++ TechChallengeFIAP/Program.cs
@@ -5,6 +5,7 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using TechChallengeFIAP.Domain.Validacao;
 using TechChallengeFIAP.Application.Services;
+using Microsoft.OpenApi.Models;
 
 Env.Load();
 
@@ -21,7 +22,32 @@ builder.Services.AddControllers();
 builder
     .Services
     .AddEndpointsApiExplorer()
-    .AddSwaggerGen()
+    .AddSwaggerGen(c =>
+    {
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n Enter 'Bearer {SeuToken}' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+    })
     .AddBasicServices(builder.Configuration)
     .AddProblemDetailsForModelRequestValidation();
 
